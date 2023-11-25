620a6bf7d800a1ca1c103e5706fbf83b882de132
Yuya Sakamoto <yuya.s713@gmail.com>
2023-11-25 13:06:06 +0900

    データのやりとりができるのを確認した

---
+++ local_Chat_Messenger/client.py
@@ -0,0 +1,34 @@
+import socket
+import sys
+
+sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+
+server_address = "/tmp/socket_file"
+print(f"conncting to {server_address}")
+
+try:
+    sock.connect(server_address)
+except socket.error as err:
+    print(err)
+    sys.exit(1)
+
+try:
+    message = input("tweet: ")
+    print(message.encode("utf-8"))
+    print(message)
+    sock.sendall(message.encode("utf-8"))
+    sock.settimeout(2)
+    try:
+        while True:
+            data = sock.recv(4096)
+
+            if data:
+                print("Server response:", data)
+            else:
+                break
+    except TimeoutError as err:
+        print(err)
+
+finally:
+    print("closing socket")
+    sock.close()
